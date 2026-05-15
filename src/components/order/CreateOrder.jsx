87222d224bfd744b8b999ff8176dd267956c2482
adishetty19 <shettyaditya19@gmail.com>
2026-05-15 13:26:04 +0530

    seventh-commit

--- src/components/order/CreateOrder.jsx
+++ src/components/order/CreateOrder.jsx
@@ -1,10 +1,11 @@
 import { useSelector } from "react-redux";
 import Button from "../../ui/Button/Button";
-import { getCart, getTotalPrice } from "../cart/cartSlice";
+import { clearCart, getCart, getTotalPrice } from "../cart/cartSlice";
 import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router";
 import { createOrder } from "../../services/apiRestaurent";
 import { formatCurrency } from "../../utilities/helpers";
+import store from "../../services/state";
 
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
@@ -69,12 +70,6 @@ export default function CreateOrder() {
               id="address"
               required
             />
-            {/* <button
-              type="button"
-              className="absolute right-1 top-1/2 -translate-y-1/2 rounded-full bg-yellow-400 px-4 py-2 text-xs font-semibold font-mono uppercase tracking-wide text-stone-800 hover:bg-yellow-300"
-            >
-              Get Position
-            </button> */}
           </div>
         </div>
 
@@ -123,6 +118,7 @@ export async function action({ request }) {
   console.log(newOrder);
 
   const order = await createOrder(newOrder);
+  store.dispatch(clearCart());
   console.log(order.data.id);
   return redirect(`/order/${order.data.id}`);
 }
